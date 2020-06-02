6c1f6b45fc39235c1ce508eed0f05a3f3cee3d4e
Darth9 <reyprimerjefe@gmail.com>
2020-06-02 14:43:20 +0100

    Update kernel cmdlines; don't use LZMA compression, etc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,13 +45,11 @@ TARGET_BOARD_PLATFORM := sm6150
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno612
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1
-BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 loop.max_part=7
-BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket androidboot.usbconfigfs=true androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 ehci-hcd.park=3 service_locator.enable=1
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000
+BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7 swiotlb=2048 androidboot.selinux=permissive
 
-# Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.usbconfigfs=true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -62,7 +60,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 BOARD_KERNEL_SEPARATED_DTBO := true
 ifeq ($(FOX_BUILD_FULL_KERNEL_SOURCES),1)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/violet
-TARGET_KERNEL_CONFIG := vendor/violet-perf_fox_defconfig
+TARGET_KERNEL_CONFIG := vendor/violet-fox_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 else
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
@@ -112,9 +110,6 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_FBE := true
 
-# lzma
-LZMA_RAMDISK_TARGETS := recovery
-
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
@@ -137,6 +132,7 @@ TW_HAS_EDL_MODE := true
 PLATFORM_SECURITY_PATCH := 2099-12-31
 #
 AB_OTA_UPDATER := false
+TW_EXCLUDE_SUPERSU := false
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TW_INCLUDE_CRYPTO_FBE := true
